1a1871f8509694ffda753006e26a5471677efb9f
Sion Rivas <sion.rivas489@gmail.com>
2025-02-13 00:47:25 -0600

    mini fix

--- config/site.ts
+++ config/site.ts
@@ -31,4 +31,11 @@ export const siteConfig = {
       href: '/cursos',
     },
   ],
+  links: {
+    github: 'https://github.com/heroui-inc/heroui',
+    twitter: 'https://twitter.com/hero_ui',
+    docs: 'https://heroui.com',
+    discord: 'https://discord.gg/9b6yyZKmH4',
+    sponsor: 'https://patreon.com/jrgarciadev',
+  },
 };
